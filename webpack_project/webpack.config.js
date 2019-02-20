eccd10235a961645d3ac0795bcdbc9911f12cc37
shenwl <shenweilun96@gmail.com>
2019-02-20 13:09:11 +0800

    一个最简单的webpack配置，index文件转移

--- webpack_project/webpack.config.js
+++ webpack_project/webpack.config.js
@@ -1,3 +1,9 @@
+const {resolve} = require('path');
+
 module.exports = {
-  entry: './src/index.js'
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: resolve(__dirname, './dist'),
+  }
 };
