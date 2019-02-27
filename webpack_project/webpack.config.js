3bd267714d1966f288ac6e735f47384adfcf6564
shenwl <shenweilun96@gmail.com>
2019-02-27 18:52:14 +0800

    通过占位符，让打包出的文件名后加上hash

--- webpack_project/webpack.config.js
+++ webpack_project/webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
         use: {
           loader: 'file-loader',
           options: {
-            name: '[name].[ext]',
+            name: '[name]_[hash].[ext]',
           }
         }
       }
