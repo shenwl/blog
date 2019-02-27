78992143cc8fa3b2b8f705e3917825f13b939d7f
shenwl <shenweilun96@gmail.com>
2019-02-27 21:34:43 +0800

    修复loader顺序

--- webpack_project/webpack.config.js
+++ webpack_project/webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'postcss-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
     ]
   },
