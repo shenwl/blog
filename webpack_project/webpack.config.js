b44cb78766a87222b15dab3c250c9d930ea0880a
shenwl <shenweilun96@gmail.com>
2019-03-04 19:11:51 +0800

    devtool: 'source-map' 配置，不需要也可设置为'none'

--- webpack_project/webpack.config.js
+++ webpack_project/webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   // mode默认是production，但是不写会报warning
   // mode设置development, 代码不会压缩
   mode: 'development',
+  devtool: 'source-map',
   entry: {
     main: './src/index.js',
   },
