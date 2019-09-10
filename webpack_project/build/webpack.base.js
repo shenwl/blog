086f47f06fed82ba8da77bc035e117582be72443
shenwl <shenweilun96@gmail.com>
2019-09-10 23:21:08 +0800

    shimming：webpack.ProvidePlugin垫片

--- webpack_project/build/webpack.base.js
+++ webpack_project/build/webpack.base.js
@@ -1,15 +1,16 @@
-const { resolve } = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { resolve } = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
   entry: {
-    main: './src/index.js',
+    main: "./src/index.js",
   },
   output: {
-    filename: '[name].[contenthash].js',
-    chunkFilename: '[name].[contenthash].chunk.js',
-    path: resolve(__dirname, '../dist'),
+    filename: "[name].[contenthash].js",
+    chunkFilename: "[name].[contenthash].chunk.js",
+    path: resolve(__dirname, "../dist"),
   },
   module: {
     rules: [
@@ -21,34 +22,38 @@ module.exports = {
       {
         test: /\.(png|jpg|gif|jpeg)$/,
         use: {
-          loader: 'url-loader',
+          loader: "url-loader",
           options: {
-            name: '[name]_[hash].[ext]',
-            outputPath: 'img/',
-            limit: 2048
-          }
-        }
+            name: "[name]_[hash].[ext]",
+            outputPath: "img/",
+            limit: 2048,
+          },
+        },
       },
       {
         test: /\.(eot|ttf|svg)$/,
         use: {
-          loader: 'file-loader'
-        }
+          loader: "file-loader",
+        },
       },
-    ]
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: resolve(__dirname, '../index.html'),
+      template: resolve(__dirname, "../index.html"),
     }),
-    new CleanWebpackPlugin([resolve(__dirname, '../dist')], {
-      root: resolve(__dirname, '../'),
+    new CleanWebpackPlugin([resolve(__dirname, "../dist")], {
+      root: resolve(__dirname, "../"),
     }),
+    new webpack.ProvidePlugin({
+      // 当发现一个模块里用了$，就会在该模块引入jquery
+      $: 'jquery',
+    })
   ],
   optimization: {
     usedExports: true,
     splitChunks: {
-      chunks: 'all',
+      chunks: "all",
     },
   },
 };
