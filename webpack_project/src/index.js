336a66756abb36dd433c3216dce5bcfbe8bb45ea
shenwl <shenweilun96@gmail.com>
2019-10-20 23:41:11 +0800

    测试代码支持pwa

--- webpack_project/src/index.js
+++ webpack_project/src/index.js
@@ -1,1 +1,11 @@
 console.log(this === window)
+
+if('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('register success ', registration)
+    }).catch(error => {
+      console.error('register error ', error)
+    })
+  })
+}
