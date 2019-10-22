921759e054efe64e6d9808ff3c37d0b0ff6df984
shenwl <shenweilun96@gmail.com>
2019-10-22 21:35:34 +0800

    引入更多包检测打包速度

--- webpack_project/src/index.tsx
+++ webpack_project/src/index.tsx
@@ -1,4 +1,6 @@
 import * as _ from 'lodash';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
 interface Person {
   name: string;
@@ -9,5 +11,12 @@ function sayName(person: Person): void {
   console.log(_.join(['i am', person.name]));
 }
 
-
 sayName({name: 'allen', age: 21})
+
+function Comp() {
+  return (
+    <div>Comp</div>
+  )
+}
+
+ReactDOM.render(<Comp />, document.getElementById('root'));
