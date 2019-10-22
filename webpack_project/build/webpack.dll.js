81029a125409190b7d285bae31f5562a66dc7796
shenwl <shenweilun96@gmail.com>
2019-10-22 20:54:39 +0800

    yarn add add-asset-html-webpack-plugin & build:dll

---
+++ webpack_project/build/webpack.dll.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    vendors: ['react', 'react-dom', 'lodash'],
+  },
+  output: {
+    filename: '[name].dll.js',
+    path: path.resolve(__dirname, '../dist'),
+    library: '[name]',
+  }
+}
